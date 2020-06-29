c668058c3aaedd6e8416b05c9643b265f53a63ca
victor <victor@thecodingpapa.com>
2020-06-29 11:45:07 +0900

    11 feed screen에 Listview

--- lib/feed_screen.dart
+++ lib/feed_screen.dart
@@ -10,6 +10,17 @@ class FeedScreen extends StatelessWidget {
           style: TextStyle(fontFamily: 'VeganStyle', color: Colors.black87),
         ),
       ),
+      body: ListView.builder(
+        itemBuilder: feedListBuilder,
+        itemCount: 30,
+      ),
+    );
+  }
+
+  Widget feedListBuilder(BuildContext context, int index) {
+    return Container(
+      color: Colors.accents[index % Colors.accents.length],
+      height: 100,
     );
   }
 }
