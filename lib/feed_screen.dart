4cfb980c096eeb1f8e74dc6da746bf5a9d581cf4
victor <victor@thecodingpapa.com>
2020-06-30 09:16:20 +0900

    12 appbar(actionbar) 만들어보자

--- lib/feed_screen.dart
+++ lib/feed_screen.dart
@@ -1,13 +1,39 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class FeedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
+      appBar: CupertinoNavigationBar(
+        leading: IconButton(
+            onPressed: null,
+            icon: Icon(
+              Icons.camera_alt,
+              color: Colors.black87,
+            )),
+        middle: Text(
           'instagram',
           style: TextStyle(fontFamily: 'VeganStyle', color: Colors.black87),
+        ),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            IconButton(
+              onPressed: null,
+              icon: ImageIcon(
+                AssetImage('assets/images/actionbar_camera.png'),
+                color: Colors.black87,
+              ),
+            ),
+            IconButton(
+              onPressed: null,
+              icon: ImageIcon(
+                AssetImage('assets/images/actionbar_camera.png'),
+                color: Colors.black87,
+              ),
+            ),
+          ],
         ),
       ),
       body: ListView.builder(
