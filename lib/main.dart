f25f578ff2fab50b2b56718fb6e7e450a16a864b
victor <victor@thecodingpapa.com>
2020-07-03 12:51:04 +0900

    36 animated switcher (sign up form, sign in form)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:instagramtworecord/constants/material_white.dart';
 import 'package:instagramtworecord/home_page.dart';
+import 'package:instagramtworecord/screens/auth_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: AuthScreen(),
+//      home: HomePage(),
       theme: ThemeData(primarySwatch: white),
     );
   }
--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:instagramtworecord/screens/profile_screen.dart';
+import 'package:instagramtworecord/widgets/sign_in_form.dart';
+import 'package:instagramtworecord/widgets/sign_up_form.dart';
 
 class AuthScreen extends StatefulWidget {
   @override
@@ -6,8 +9,44 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  Widget signUpForm = SignUpForm();
+  Widget signInForm = SignInForm();
+
+  Widget currentWidget;
+
+  @override
+  void initState() {
+    if (currentWidget == null) currentWidget = signUpForm;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            AnimatedSwitcher(
+              child: currentWidget,
+              duration: duration,
+            ),
+            Container(
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    if (currentWidget is SignUpForm) {
+                      currentWidget = signInForm;
+                    } else {
+                      currentWidget = signUpForm;
+                    }
+                  });
+                },
+                child: Text('go to Sign up'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
