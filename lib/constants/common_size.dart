eb6b41290953acd52d5a6d38242eea88f4a054fa
victor <victor@thecodingpapa.com>
2020-07-08 10:58:58 +0900

    41 TextFormField 사용해서 email input 만들자

--- lib/constants/common_size.dart
+++ lib/constants/common_size.dart
@@ -1,5 +1,6 @@
 const double common_l_gap = 16.0;
 const double common_gap = 14.0;
+const double common_s_gap = 12.0;
 const double common_xxs_gap = 8.0;
 
 const double avatar_size = 30.0;
--- lib/widgets/sign_up_form.dart
+++ lib/widgets/sign_up_form.dart
@@ -9,20 +9,49 @@ class SignUpForm extends StatefulWidget {
 class _SignUpFormState extends State<SignUpForm> {
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
+  TextEditingController _emailController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: ListView(
-        children: <Widget>[
-          SizedBox(
-            height: common_l_gap,
-          ),
-          Image.asset('assets/images/insta_text_logo.png'),
-          TextFormField(),
-          TextFormField(),
-          TextFormField(),
-        ],
+    return Padding(
+      padding: const EdgeInsets.all(common_gap),
+      child: Form(
+        key: _formKey,
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              height: common_l_gap,
+            ),
+            Image.asset('assets/images/insta_text_logo.png'),
+            TextFormField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                  hintText: 'Email',
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey[300],
+                      ),
+                      borderRadius: BorderRadius.circular(common_s_gap)),
+                  filled: true,
+                  fillColor: Colors.grey[100]),
+              validator: (text) {
+                if (text.isNotEmpty && text.contains("@")) {
+                  return null;
+                } else {
+                  return '정확한 이메일 주소를 입력해주세용~';
+                }
+              },
+            ),
+            TextFormField(),
+            TextFormField(),
+          ],
+        ),
       ),
     );
   }
