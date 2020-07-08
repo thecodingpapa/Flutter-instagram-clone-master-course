35ef327aff6dab0ad9c707bffdb1619c93e43c9b
victor <victor@thecodingpapa.com>
2020-07-08 12:21:48 +0900

    47 커스텀 Divider

--- lib/widgets/sign_up_form.dart
+++ lib/widgets/sign_up_form.dart
@@ -95,6 +95,33 @@ class _SignUpFormState extends State<SignUpForm> {
                 ),
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(6)),
+              ),
+              SizedBox(
+                height: common_s_gap,
+              ),
+              Stack(
+                alignment: Alignment.center,
+                children: <Widget>[
+                  Positioned(
+                    left: 0,
+                    right: 0,
+                    height: 1,
+                    child: Container(
+                      color: Colors.grey[300],
+                      height: 1,
+                    ),
+                  ),
+                  Container(
+                    color: Colors.grey[50],
+                    height: 3,
+                    width: 60,
+                  ),
+                  Text(
+                    'OR',
+                    style: TextStyle(
+                        color: Colors.grey[500], fontWeight: FontWeight.bold),
+                  )
+                ],
               )
             ],
           ),
