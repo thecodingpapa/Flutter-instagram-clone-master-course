4c1ab2fc9825bae1e7a709873b321dfac0f35669
victor <victor@thecodingpapa.com>
2020-07-12 13:04:26 +0900

    53 BottomNavigationBar without Icon

--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -6,10 +6,32 @@ class CameraScreen extends StatefulWidget {
 }
 
 class _CameraScreenState extends State<CameraScreen> {
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.cyanAccent,
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        iconSize: 0,
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black54,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.radio_button_checked), title: Text('GALLERY')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.radio_button_checked), title: Text('PHOTO')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.radio_button_checked), title: Text('VIDEO')),
+        ],
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          print(index);
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
     );
   }
 }
