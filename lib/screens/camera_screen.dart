c827a15842319130c27186c6c4e47123198fa3cf
victor <victor@thecodingpapa.com>
2020-07-12 13:34:16 +0900

    54 Link Pageview and BottomNavBar

--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -7,10 +7,36 @@ class CameraScreen extends StatefulWidget {
 
 class _CameraScreenState extends State<CameraScreen> {
   int _currentIndex = 0;
+  PageController _pageController = PageController();
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        children: <Widget>[
+          Container(
+            color: Colors.cyanAccent,
+          ),
+          Container(
+            color: Colors.amberAccent,
+          ),
+          Container(
+            color: Colors.greenAccent,
+          ),
+        ],
+        onPageChanged: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
       bottomNavigationBar: BottomNavigationBar(
         iconSize: 0,
         selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
@@ -25,13 +51,17 @@ class _CameraScreenState extends State<CameraScreen> {
               icon: Icon(Icons.radio_button_checked), title: Text('VIDEO')),
         ],
         currentIndex: _currentIndex,
-        onTap: (index) {
-          print(index);
-          setState(() {
-            _currentIndex = index;
-          });
-        },
+        onTap: _onItemTabbed,
       ),
     );
+  }
+
+  void _onItemTabbed(index) {
+    print(index);
+    setState(() {
+      _currentIndex = index;
+      _pageController.animateToPage(_currentIndex,
+          duration: Duration(milliseconds: 200), curve: Curves.fastOutSlowIn);
+    });
   }
 }
