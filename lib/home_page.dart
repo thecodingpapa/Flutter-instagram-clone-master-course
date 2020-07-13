7b96cc4f9884caf95855610a91beea0d977dc9c1
victor <victor@thecodingpapa.com>
2020-07-13 11:08:28 +0900

    59 Snackbar는 반드시 Scaffold 안에서 사용해야되

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -24,6 +24,7 @@ class _HomePageState extends State<HomePage> {
   ];
 
   int _selectedIndex = 0;
+  GlobalKey<ScaffoldState> _key = GlobalKey<ScaffoldState>();
 
   static List<Widget> _screens = <Widget>[
     FeedScreen(),
@@ -43,6 +44,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     if (size == null) size = MediaQuery.of(context).size;
     return Scaffold(
+      key: _key,
       body: IndexedStack(
         index: _selectedIndex,
         children: _screens,
@@ -84,11 +86,11 @@ class _HomePageState extends State<HomePage> {
         action: SnackBarAction(
           label: 'OK',
           onPressed: () {
-            Scaffold.of(context).hideCurrentSnackBar();
+            _key.currentState.hideCurrentSnackBar();
           },
         ),
       );
-      Scaffold.of(context).showSnackBar(snackBar);
+      _key.currentState.showSnackBar(snackBar);
     }
   }
 
