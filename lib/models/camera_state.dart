da2b9fffc7abfb41aa8fd7ec03880eaac028709d
victor <victor@thecodingpapa.com>
2020-07-16 14:06:36 +0900

    66 create camera state model(change notifier model)

--- lib/models/camera_state.dart
+++ lib/models/camera_state.dart
@@ -5,4 +5,38 @@ class CameraState extends ChangeNotifier {
   CameraController _controller;
   CameraDescription _cameraDescription;
   bool _readyTakePhoto = false;
+
+  void getReadyToTakePhoto() async {
+    List<CameraDescription> cameras = await availableCameras();
+
+    if (cameras != null && cameras.isNotEmpty) {
+      setCameraDescription(cameras[0]);
+    }
+
+    bool init = false;
+    while (!init) {
+      init = await initialize();
+    }
+
+    _readyTakePhoto = true;
+    notifyListeners();
+  }
+
+  void setCameraDescription(CameraDescription cameraDescription) {
+    _cameraDescription = cameraDescription;
+    _controller = CameraController(_cameraDescription, ResolutionPreset.medium);
+  }
+
+  Future<bool> initialize() async {
+    try {
+      await _controller.initialize();
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  CameraController get controller => _controller;
+  CameraDescription get description => _cameraDescription;
+  bool get isReadyToTakePhoto => _readyTakePhoto;
 }
