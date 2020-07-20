3cc3ac524b5e613adf34b8dcac5d070f0172f925
victor <victor@thecodingpapa.com>
2020-07-20 15:09:30 +0900

    76 localimage class to bytes to file

--- lib/widgets/my_gallery.dart
+++ lib/widgets/my_gallery.dart
@@ -1,6 +1,13 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:instagramtworecord/models/gallery_state.dart';
+import 'package:instagramtworecord/screens/share_post_screen.dart';
 import 'package:local_image_provider/device_image.dart';
+import 'package:local_image_provider/local_image.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 
 class MyGallery extends StatefulWidget {
@@ -15,17 +22,38 @@ class _MyGalleryState extends State<MyGallery> {
       builder: (BuildContext context, GalleryState galleryState, Widget child) {
         return GridView.count(
           crossAxisCount: 3,
-          children: getImages(galleryState),
+          children: getImages(context, galleryState),
         );
       },
     );
   }
 
-  List<Widget> getImages(GalleryState galleryState) {
+  List<Widget> getImages(BuildContext context, GalleryState galleryState) {
     return galleryState.images
-        .map((localImage) => Image(
-              image: DeviceImage(localImage),
+        .map((localImage) => InkWell(
+              onTap: () async {
+                Uint8List bytes = await localImage.getScaledImageBytes(
+                    galleryState.localImageProvider, 0.3);
+
+                final String timeInMilli =
+                    DateTime.now().millisecondsSinceEpoch.toString();
+                try {
+                  final path = join(
+                      (await getTemporaryDirectory()).path, '$timeInMilli.png');
+                  File imageFile = File(path)..writeAsBytesSync(bytes);
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (_) => SharePostScreen(imageFile)));
+                } catch (e) {}
+              },
+              child: Image(
+                image: DeviceImage(localImage),
+              ),
             ))
         .toList();
+  }
+
+  Future<Uint8List> localImageToBytes(
+      GalleryState galleryState, LocalImage localImage) {
+    return localImage.getScaledImageBytes(galleryState.localImageProvider, 0.3);
   }
 }
