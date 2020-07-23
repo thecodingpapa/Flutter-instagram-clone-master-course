e487b7cf35a382c9d57ace3c4102f4eb413c71ea
victor <victor@thecodingpapa.com>
2020-07-23 13:05:29 +0900

    88 error handling for sign up

--- lib/models/firebase_auth_state.dart
+++ lib/models/firebase_auth_state.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 
 class FirebaseAuthState extends ChangeNotifier {
   FirebaseAuthStatus _firebaseAuthStatus = FirebaseAuthStatus.signout;
@@ -17,9 +18,31 @@ class FirebaseAuthState extends ChangeNotifier {
     });
   }
 
-  void registerUser({@required String email, @required String password}) {
-    _firebaseAuth.createUserWithEmailAndPassword(
-        email: email, password: password);
+  void registerUser(BuildContext context,
+      {@required String email, @required String password}) {
+    _firebaseAuth
+        .createUserWithEmailAndPassword(
+            email: email.trim(), password: password.trim())
+        .catchError((error) {
+      print(error);
+      String _message = "";
+      switch (error.code) {
+        case 'ERROR_WEAK_PASSWORD':
+          _message = "패스워드 잘 넣어줘!!";
+          break;
+        case 'ERROR_INVALID_EMAIL':
+          _message = "이멜 주소가 좀 이상해!";
+          break;
+        case 'ERROR_EMAIL_ALREADY_IN_USE':
+          _message = "해당 이멜은 다른 사람이 쓰고있네??";
+          break;
+      }
+
+      SnackBar snackBar = SnackBar(
+        content: Text(_message),
+      );
+      Scaffold.of(context).showSnackBar(snackBar);
+    });
   }
 
   void login({@required String email, @required String password}) {
--- lib/widgets/sign_up_form.dart
+++ lib/widgets/sign_up_form.dart
@@ -114,7 +114,9 @@ class _SignUpFormState extends State<SignUpForm> {
         if (_formKey.currentState.validate()) {
           print('Validation success!!');
           Provider.of<FirebaseAuthState>(context, listen: false).registerUser(
-              email: _emailController.text, password: _pwController.text);
+              context,
+              email: _emailController.text,
+              password: _pwController.text);
         }
       },
       child: Text(
