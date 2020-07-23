04cebbf7a88dac09461a45c734d69da325d00693
victor <victor@thecodingpapa.com>
2020-07-23 09:04:44 +0900

    79 change color when click a button

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -7,12 +7,19 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  List<bool> followings = List.generate(30, (index) => false);
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: ListView.separated(
             itemBuilder: (context, index) {
               return ListTile(
+                onTap: () {
+                  setState(() {
+                    followings[index] = !followings[index];
+                  });
+                },
                 leading: RoundedAvatar(),
                 title: Text('username $index'),
                 subtitle: Text('user bio number $index'),
@@ -21,8 +28,10 @@ class _SearchScreenState extends State<SearchScreen> {
                   width: 80,
                   alignment: Alignment.center,
                   decoration: BoxDecoration(
-                    color: Colors.red[50],
-                    border: Border.all(color: Colors.red, width: 0.5),
+                    color: followings[index] ? Colors.red[50] : Colors.blue[50],
+                    border: Border.all(
+                        color: followings[index] ? Colors.red : Colors.blue,
+                        width: 0.5),
                     borderRadius: BorderRadius.circular(8),
                   ),
                   child: Text(
@@ -38,6 +47,6 @@ class _SearchScreenState extends State<SearchScreen> {
                 color: Colors.grey,
               );
             },
-            itemCount: 30));
+            itemCount: followings.length));
   }
 }
