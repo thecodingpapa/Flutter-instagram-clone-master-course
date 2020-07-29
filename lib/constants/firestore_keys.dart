bc75910b3954e645c610694e86ed2cbb4f7512e3
victor <victor@thecodingpapa.com>
2020-07-29 09:50:47 +0900

    96 create firestore keys

---
+++ lib/constants/firestore_keys.dart
@@ -0,0 +1,25 @@
+const COLLECTION_USERS = 'Users';
+const COLLECTION_POSTS = 'Posts';
+const COLLECTION_COMMENTS = 'Comments';
+
+const KEY_USERKEY = 'user_key';
+const KEY_PROFILEIMG = 'profile_img';
+const KEY_EMAIL = 'email';
+const KEY_LIKEDPOSTS = 'liked_posts';
+const KEY_FOLLOWERS = 'followers';
+const KEY_FOLLOWINGS = 'followings';
+const KEY_MYPOSTS = 'my_posts';
+const KEY_USERNAME = 'username';
+
+const KEY_COMMENT = 'comment';
+const KEY_COMMENTTIME = 'commenttime';
+
+const KEY_POSTIMG = 'postimg';
+const KEY_POSTURI = 'posturi';
+const KEY_CAPTION = 'caption';
+const KEY_LASTCOMMENT = 'last_comment';
+const KEY_LASTCOMMENTOR = 'last_commentor';
+const KEY_LASTCOMMENTTIME = 'last_commenttime';
+const KEY_NUMOFLIKES = 'num_of_likes';
+const KEY_NUMOFCOMMENTS = 'num_of_comments';
+const KEY_POSTTIME = 'post_time';
