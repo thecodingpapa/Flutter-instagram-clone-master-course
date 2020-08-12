fdd0b3430d4def1bac170e0646b8204dd9652c5e
victor <victor@thecodingpapa.com>
2020-08-12 10:21:20 +0900

    126 create transaction to upload post data

---
+++ lib/repo/post_network_repository.dart
@@ -0,0 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:instagramtworecord/constants/firestore_keys.dart';
+
+class PostNetworkRepository {
+  Future<Map<String, dynamic>> createNewPost(
+      String postKey, Map<String, dynamic> postData) async {
+    final DocumentReference postRef =
+        Firestore.instance.collection(COLLECTION_POSTS).document(postKey);
+    final DocumentSnapshot postSnapshot = await postRef.get();
+    final DocumentReference userRef = Firestore.instance
+        .collection(COLLECTION_USERS)
+        .document(postData[KEY_USERKEY]);
+
+    return Firestore.instance.runTransaction((Transaction tx) async {
+      if (!postSnapshot.exists) {
+        await tx.set(postRef, postData);
+        await tx.update(userRef, {
+          KEY_MYPOSTS: FieldValue.arrayUnion([postKey])
+        });
+      }
+    });
+  }
+}
+
+PostNetworkRepository postNetworkRepository = PostNetworkRepository();
