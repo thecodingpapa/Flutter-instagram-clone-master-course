3898d5c802529f7cc56a2a38f0a1884d21ad338d
victor <victor@thecodingpapa.com>
2020-08-20 12:37:00 +0900

    141 fix stream provider issue

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -19,9 +19,8 @@ class FeedScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<List<PostModel>>(
-      create: (BuildContext context) =>
-          postNetworkRepository.fetchPostsFromAllFollowers(followings),
+    return StreamProvider<List<PostModel>>.value(
+      value: postNetworkRepository.fetchPostsFromAllFollowers(followings),
       child: Consumer<List<PostModel>>(
           builder: (BuildContext context, List<PostModel> posts, Widget child) {
         if (posts == null || posts.isEmpty)
