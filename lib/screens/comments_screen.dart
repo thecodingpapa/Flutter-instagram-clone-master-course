5f0dfc83433873771b70d5e65bcd304bf95786fd
victor <victor@thecodingpapa.com>
2020-08-23 15:07:44 +0900

    148 comments page input layout

--- lib/screens/comments_screen.dart
+++ lib/screens/comments_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagramtworecord/constants/common_size.dart';
 
 class CommentsScreen extends StatefulWidget {
   @override
@@ -6,10 +7,53 @@ class CommentsScreen extends StatefulWidget {
 }
 
 class _CommentsScreenState extends State<CommentsScreen> {
+  TextEditingController _textEditingController = TextEditingController();
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.amber,
+    return Scaffold(
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            Expanded(
+                child: Container(
+              color: Colors.amber,
+            )),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: common_gap),
+                    child: TextFormField(
+                      controller: _textEditingController,
+                      cursorColor: Colors.black54,
+                      decoration: InputDecoration(
+                          hintText: 'Add a comment...',
+                          border: InputBorder.none),
+                      validator: (String value) {
+                        if (value.isEmpty)
+                          return 'Input something';
+                        else
+                          return null;
+                      },
+                    ),
+                  ),
+                ),
+                FlatButton(
+                  onPressed: () {
+                    if (_formKey.currentState.validate()) {
+                      //todo: push a comment
+                    }
+                  },
+                  child: Text("Post"),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
