5156e3b708807afca0120f9f4c7cae42d77e0e97
victor <victor@thecodingpapa.com>
2020-08-23 17:23:15 +0900

    153 connect like button to firestore

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:instagramtworecord/constants/common_size.dart';
 import 'package:instagramtworecord/constants/screen_size.dart';
 import 'package:instagramtworecord/models/firestore/post_model.dart';
+import 'package:instagramtworecord/models/user_model_state.dart';
 import 'package:instagramtworecord/repo/image_network_repository.dart';
+import 'package:instagramtworecord/repo/post_network_repository.dart';
 import 'package:instagramtworecord/screens/comments_screen.dart';
 import 'package:instagramtworecord/widgets/comment.dart';
 import 'package:instagramtworecord/widgets/my_progress_indicator.dart';
@@ -89,10 +91,24 @@ class Post extends StatelessWidget {
           color: Colors.black87,
         ),
         Spacer(),
-        IconButton(
-          onPressed: null,
-          icon: ImageIcon(AssetImage('assets/images/heart_selected.png')),
-          color: Colors.black87,
+        Consumer<UserModelState>(
+          builder: (BuildContext context, UserModelState userModelState,
+              Widget child) {
+            return IconButton(
+              onPressed: () {
+                postNetworkRepository.toggleLike(
+                    postModel.postKey, userModelState.userModel.userKey);
+              },
+              icon: ImageIcon(
+                AssetImage(postModel.numOfLikes
+                        .contains(userModelState.userModel.userKey)
+                    ? 'assets/images/heart_selected.png'
+                    : 'assets/images/heart.png'),
+                color: Colors.redAccent,
+              ),
+              color: Colors.black87,
+            );
+          },
         ),
       ],
     );
